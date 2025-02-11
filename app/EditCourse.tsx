2ee2fb919546079a0bdcdb87c872296d068271f3
mccollimcyrus <mccollimcyrus@gmail.com>
2025-02-10 20:04:14 -0500

    remove dirs

---
+++ app/EditCourse.tsx
@@ -0,0 +1,21 @@
+/**
+ * Screen: EditCourse
+ * @author Cyrus M. // Last updated by.
+ * @version 0.1.0 02/01/25
+ */
+
+import React from "react";
+
+import {
+    View,
+    Text
+} from 'react-native'
+
+
+export const EditCourse = () => {
+    return (
+        <View>
+            <Text>EditCourse</Text>
+        </View>
+    )
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,39 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+/**
+ * App Navigation Handler
+    * Root of the application.
+ * @author Cyrus M. // Last updated by.
+ * @version 0.1.0 02/01/25
+ */
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import React from 'react';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { Calendar } from './Calendar';
+import { Profile } from './Profile';
+import { EditTask } from './EditTask';
+import { EditEvent } from './EditEvent';
+import { Courses } from './Courses';
+
+export type RootStackParameters = {
+  Calendar : undefined,
+  Profile : undefined,
+  EditTask: undefined,
+  EditEvent : undefined,
+  Courses : undefined
+}
+
+const Stack = createNativeStackNavigator<RootStackParameters>();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+      <Stack.Navigator initialRouteName='Calendar'>
+        <Stack.Screen name="Calendar" component={Calendar}/>
+        <Stack.Screen name="EditEvent" component={EditEvent}/>
+        <Stack.Screen name="EditTask" component={EditTask}/>
+        <Stack.Screen name="Profile" component={Profile}/>
+        <Stack.Screen name="Courses" component={Courses}/>
+      </Stack.Navigator>
   );
 }
