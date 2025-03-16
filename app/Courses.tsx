0868b241ad07596a5fc39bfd9ef57a581d91086f
mushtariy <mushtarit080904@gmail.com>
2025-03-16 11:56:19 -0400

    Need to debug

--- app/Courses.tsx
+++ app/Courses.tsx
@@ -1,20 +1,69 @@
 /**
  * Screen: Courses
- * @author Cyrus M. // Last updated by.
+ * @author Mushtariy I . // Last updated by.
  * @version 0.1.0 02/01/25
  */
 
-import React from "react";
+import React, {useState} from "react";
+import {View, Text, FlatList, TouchableOpacity, StyleSheet} from 'react-native';
+import {Ionicons} from '@expo/vector-icons'; //icons library
+import { useNavigation } from "expo-router";
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
-import {
-    View,
-    Text
-} from 'react-native'
-
-export const Courses = () => {
-    return (
-        <View>
-            <Text>Courses</Text>
-        </View>
-    )
+interface Course { //course type
+    id: string;
+    name: string;
+    code: string;
+    time: string;
+    location: string;
+    type: Course;
 }
+
+type RootStackParamList = { //navigation parameters
+    Courses: undefined; 
+    EditCourse: {addCourse: (course: Course) => void}; //connects two pages
+    
+};
+export const Courses = () => {
+    const [courses, setCourses] = useState<Course[]>([]);
+    const addCourse = (newCourse: Course) => {
+        setCourses([...courses, newCourse]);
+    };
+    return (
+        <View style={styles.container}>
+            <Text style={styles.header}>Courses</Text>
+            {courses.length === 0 ? (
+                <Text style={styles.emptyMessage}>No Course available</Text>
+            ) : (
+            <FlatList 
+                data={courses}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) =>  (
+                    <View style={styles.courseCard}>
+                        <Text style={styles.courseName}>{item.name}</Text>
+                        <Text >{item.code}</Text>
+                        <Text >{item.time}</Text>
+                    </View>
+                )}
+            />
+            )}
+
+            <TouchableOpacity
+                onPress={() => navigator.navigate('EditCourse', {addCourse})}
+                style={styles.addButton}
+            >
+                <Ionicons name="add-circle" size={50} color="white"/>
+                <Text style={styles.addButtonText}>Add New Course</Text>
+            </TouchableOpacity>
+      </View>                 
+     )
+}
+const styles = StyleSheet.create ({
+    container: {flex:1, padding:20, backgroundColor: '#f5f5f5', },
+    header: {fontSize: 24, fontWeight:'bold', marginBottom:10 },
+    emptyMessage: { textAlign: 'center', marginTop: 20, fontSize: 16, color: 'gray'},
+    courseCard: { backgroundColor: '#ddd', padding: 15, marginVertical: 5, borderRadius: 5},
+    courseName: { fontSize: 18, fontWeight: 'bold'},
+    addButton: { backgroundColor: 'purple', padding: 15, borderRadius: 5, alignItem: 'center', marginTop: 20},
+    addButtonText: { color: 'white', fontSize: 15 }
+});
--- app/EditCourse.tsx
+++ app/EditCourse.tsx
@@ -1,21 +1,76 @@
 /**
  * Screen: EditCourse
- * @author Cyrus M. // Last updated by.
+ * @author Mushtariy I. // Last updated by.
  * @version 0.1.0 02/01/25
  */
 
 import React from "react";
+import { } from '@react-native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {v4 as uuidv4} from 'uuid'; //identifier
 
-import {
-    View,
-    Text
-} from 'react-native'
+interface Course {
+    id: 0;
+    name: string;
+    code: string;
+    time: string;
+    location: string;
+}
 
+type RootStackParamList = {
+    Courses: undefined;
+    EditCourse: {addCourse: (course: Course) => void};
+};
 
-export const EditCourse = () => {
+type Props = NativeStackScreenProps<RootStackParamList, 'EditCourse'>;
+
+export const EditCourse = ({navigation, route}) => {
+    const [course, setCourse] = useState<Course>({ id: '', name: '', code: '', time: ''});
+
+    const handleSave = () =>{
+        if(course.name && course.code && course.time) {
+            route.params.addCourse ({...course, id: uuidv4() });
+            navigation.goBack();
+
+        }
+    };
+
     return (
-        <View>
-            <Text>EditCourse</Text>
-        </View>
+  <View style={styles.container}>
+        <Text style={styles.header}>New Course</Text>
+  
+        <Text>Course Name</Text>
+          <TextInput
+          value={course.name}
+          onChangeText={(text) => setCourse({ ...course, name: text })}
+          style={styles.input}
+           />
+  
+          <Text>Course Code</Text>
+          <TextInput
+          value={course.code}
+          onChangeText={(text) => setCourse({ ...course, code: text })}
+          style={styles.input}
+          />
+  
+         <Text>Course Time</Text>
+         <TextInput
+          value={course.time}
+          onChangeText={(text) => setCourse({ ...course, time: text })}
+          style={styles.input}
+         />
+  
+         <TouchableOpacity onPress={handleSave} style={styles.saveButton}>
+           <Text style={styles.saveButtonText}>Save Course</Text>
+         </TouchableOpacity>
+ </View>
     )
 }
+const styles = StyleSheet.create ({
+    container: { flex: 1, padding: 20, backgroundColor: '#f5f5f5' },
+    header: { fontSize: 24, fontWeight: 'bold', marginBottom: 20 },
+    input: { borderBottomWidth: 1, marginBottom: 10, paddingVertical: 5 },
+    saveButton: { backgroundColor: 'purple', padding: 10, borderRadius: 5, marginTop: 20 },
+    saveButtonText: { color: 'white', textAlign: 'center' }
+
+});
