30e5798b89829eeb8b0f67c8cabdc6868b34b6df
DaniAe <dani.ae277@gmail.com>
2025-09-30 18:43:03 -0400

    Merge branches

--- src/features/events/screens/EditEvent.tsx
+++ src/features/events/screens/EditEvent.tsx
@@ -4,18 +4,16 @@
  * @version 0.1.0 02/01/25
  */
 
-import React from "react";
+import React from 'react';
 
-import {
-    View,
-    Text
-} from 'react-native'
-
+import { View, Text } from 'react-native';
 
 export const EditEvent = () => {
-    return (
-        <View>
-            <Text>EditEvent</Text>
-        </View>
-    )
-}
+  return (
+    <View>
+      <Text>EditEvent</Text>
+    </View>
+  );
+};
+
+export default EditEvent;
